abb3d5eb5dac648b72baea220944438704f47fdb
s1290159 <83757924+s1290159@users.noreply.github.com>
2022-07-14 20:45:30 +0900

    HoT: initial revision

---
+++ Heads_or_Tails.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+#include<random>
+using namespace std;
+int main(){
+  int a=0;
+
+  srand(time(NULL));
+  cout << "Tossing a coin..." << endl;
+  for(int i=0; i<3; i++){
+    int coin = rand() % 2;
+    cout << "Round " << i+1 << ": ";
+    if(coin){
+      cout << "Heads" << endl;
+      a++;
+    }else{
+      cout << "Tails" << endl;
+    }
+  }
+  cout << "Heads: " << a << ", Tails: " << 3-a << endl;
+  return 0;
+}
